cd85c522949f05d84dccad27302fd848e3d4d2ed
YoLiMow <frank29364133@gmail.com>
2024-08-06 12:23:52 +0800

    init

---
+++ fancy-todo-list/src/components/App.jsx
@@ -0,0 +1,21 @@
+import { Box, CssBaseline } from "@mui/material";
+
+import { AppState } from "../providers/AppState.jsx";
+import { AllTodoLists } from "./AllTodoLists.jsx";
+import { AppHeader } from "./AppHeader.jsx";
+import { CurrentTodoList } from "./CurrentTodoList.jsx";
+
+function App() {
+  return (
+    <AppState>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppHeader />
+        <AllTodoLists />
+        <CurrentTodoList />
+      </Box>
+    </AppState>
+  );
+}
+
+export default App;
